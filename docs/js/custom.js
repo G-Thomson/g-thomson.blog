a19a46254c0147aa699fe1a297b499aca2d6dbe5
Geoffrey Thomson <gtho123@aucklanduni.ac.nz>
2017-04-19 19:12:56 +1200

    Edit post body padding and added footer text.

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,6 +1,16 @@
 $('.catlist a:contains("Culture")').css('background-color', '#f85c50');
 $('.catlist a:contains("Coding")').css('background-color', '#6EB7C6');
 
+function popitup(url) {
+  newwindow = window.open(url, 'name', 'height=200, width=450');
+  if (window.focus) {newwindow.focus()}
+  return false;
+}
 
+function popitup_fb(url, title) {
+  newwindow = window.open('http://www.facebook.com/share.php?u='+url, 'name', 'height=200, width=450');
+  if (window.focus) {newwindow.focus()}
+  return false;
+}
 
 
